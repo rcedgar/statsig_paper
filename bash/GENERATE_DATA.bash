2d981672b58f3ce5895ca4c1527ab851b58392ae
Robert Edgar <robert@drive5.com>
2026-02-08 10:42:00 -0800

    Add GENERATE_DATA.bash

---
+++ bash/GENERATE_DATA.bash
@@ -0,0 +1,7 @@
+./reseek27_scop40.bash
+./run_blastp.bash
+./3di_scop40.bash
+./foldseek_search_scop40.bash
+./extract_and_hist_high_score.bash
+./blastp_reseek_evalues_scatterplot.bash
+./hist_3di_selected_doms.bash
